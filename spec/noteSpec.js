4de8e127d6c84909b6cfb28d7ac23ba682c2c485
David Halewood <davidnhalewood@hotmail.com>
2017-11-08 09:43:06 +0000

    Fixing broken test

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -3,6 +3,6 @@
 aWholeNewWorld("Note", function() {
   it("is created with the note as text", function () {
     var note = new Note("This is a note!");
-    wish(note.showText()).toBeEqualTo("This is a sfdfnote!")
+    wish(note.showText()).toBeEqualTo("This is a note!")
   })
 })
