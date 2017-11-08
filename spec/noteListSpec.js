0a58c10a6c38dc119fbe8252e797e598f092e79d
David Halewood <davidnhalewood@hotmail.com>
2017-11-08 09:00:41 +0000

    Changing syntax of tests

--- spec/noteListSpec.js
+++ spec/noteListSpec.js
@@ -4,20 +4,20 @@ describe("Note List", function() {
 
   it("is created with no notes", function() {
     var noteList = new NoteList();
-    wish(noteList.allNotes()).isArray();
+    wish(noteList.allNotes()).toBeArray();
   })
  
   it("creates new notes", function () {
     var noteList = new NoteList();
     var text = "This is a note!"
     noteList.addNote(text);
-    wish(noteList.allNotes()[0]).isInstanceOf(Note);
+    wish(noteList.allNotes()[0]).toBeInstanceOf(Note);
   })
   
   it("stores notes", function () {
     var noteList = new NoteList(text);
     var text = "This is a note!"
     noteList.addNote(text)
-    wish(noteList.allNotes().length).isEqualTo(1);
+    wish(noteList.allNotes().length).toBeEqualTo(1);
   })
 })
