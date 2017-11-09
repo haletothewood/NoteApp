1b9b7d6c5b6b074e97791e710e3cd496137d445d
Aram Simonian <aram.contacts@gmail.com>
2017-11-09 17:20:58 +0000

    Adding test for abbreviated string function

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -5,4 +5,9 @@ aWholeNewWorld("Note", function() {
     var note = new Note("This is a note!");
     wish(note.showText()).toEqual("This is a note!")
   })
+
+  it('can return an abbreviated text a string of 20 characters', function () {
+    var note = new Note("This is a note! This note is more than 20 characters long!");
+    wish(note.previewText()).toEqual("This is a note! This")
+  })
 })
