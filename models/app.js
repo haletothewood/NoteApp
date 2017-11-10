3e5b6bfdc1f7f3b84dd91ae483f248a9fd68c03e
Aram Simonian <aram.contacts@gmail.com>
2017-11-10 16:28:51 +0000

    Added app.getNoteLinks for clicking on note list items

--- models/app.js
+++ models/app.js
@@ -7,10 +7,26 @@ var control = (function() {
   var saveNote = document.getElementById("save-note");
   var submitMemo = document.getElementById("write-a-note");
   var form = document.getElementById("form-for-note");
+  var noteItem = document.getElementsByTagName("li");
+  var textArea = document.getElementById("text-for-note");
 
   var init = function (){
     createEventListeners();
     getNoteText();
+  };
+
+  function getNoteLinks() {
+    var items = document.getElementsByTagName("li");
+    var itemsArray = Array.prototype.slice.call(items);
+    console.log(items);
+    console.log(itemsArray);
+
+    itemsArray.map((item, index) => {
+      item.addEventListener("click", function() {
+        form.style.display = "block";
+        textArea.value = noteList.allNotes()[index].showText();
+      })
+    });
   };
 
   function createEventListeners() {
@@ -34,6 +50,7 @@ var control = (function() {
   function showAbbreviatedNote() {
     var ul = document.getElementById("notes")
     ul.innerHTML = noteList.getAbbreviatedTextOfNotes()
+    getNoteLinks();
   }
 
   return {
